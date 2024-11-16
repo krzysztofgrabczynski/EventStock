f89cb529f2d4a8b77f90096c3d6b348452c48457
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-16 18:22:02 +0100

    Add IdentityUser and db conf into Program.cs

--- EventStock/EventStock.API/Program.cs
+++ EventStock/EventStock.API/Program.cs
@@ -1,6 +1,12 @@
+using EventStock.Domain.Models;
+using EventStock.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<Context>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,7 +20,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+} 
 
 app.UseHttpsRedirection();
 
