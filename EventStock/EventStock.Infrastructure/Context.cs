c266825d927e451b3aff89da45999fa9c2adee3b
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-16 18:19:26 +0100

    Create Context class for db

---
+++ EventStock/EventStock.Infrastructure/Context.cs
@@ -0,0 +1,18 @@
+﻿using EventStock.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventStock.Infrastructure
+{
+    public class Context : IdentityDbContext<User>
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
