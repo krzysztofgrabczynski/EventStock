d5d490c9e3734a3b410889e3813d6947ff7d210c
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-17 15:10:29 +0100

    Add default List initialization for Stocks and UserStockRoles props

--- EventStock/EventStock.Domain/Models/User.cs
+++ EventStock/EventStock.Domain/Models/User.cs
@@ -7,7 +7,7 @@ namespace EventStock.Domain.Models
     {
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
-        public ICollection<Stock>? Stocks { get; set; }
-        public ICollection<UserStockRole>? UserStockRole { get; set; }
+        public ICollection<Stock> Stocks { get; set; } = new List<Stock>();
+        public ICollection<UserStockRole> UserStockRoles { get; set; } = new List<UserStockRole>();
     }
 }
