0fffd09ad50797e9db15c22992cf6808d62fbb5b
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-17 15:13:30 +0100

    Add new props to EventEquipment model

--- EventStock/EventStock.Domain/Models/EventEquipment.cs
+++ EventStock/EventStock.Domain/Models/EventEquipment.cs
@@ -3,10 +3,12 @@
     public class EventEquipment
     {
         public int Id { get; set; }
-        
-        public int EventId { get; set; }
-        public required Event Event { get; set; }
+        public int Quantity { get; set; }
 
-        public required ICollection<Equipment> Equipments { get; set; }
+        public int EquipmentId { get; set; }
+        public int EventId { get; set; }
+
+        public required Equipment Equipment { get; set; }
+        public required Event Event { get; set; }
     }
 }
