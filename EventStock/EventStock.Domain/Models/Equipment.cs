1a00abffe851c64106d4fb649edb450a4c9784ea
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2024-11-17 15:13:53 +0100

    Add default init for EventEquipments prop

--- EventStock/EventStock.Domain/Models/Equipment.cs
+++ EventStock/EventStock.Domain/Models/Equipment.cs
@@ -4,13 +4,10 @@
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-
-        public int StockId { get; set; }
         public required Stock Stock { get; set; }
-    
         public string? Brand { get; set; }
         public required Category Category { get; set; }
         public int Quantity { get; set; }
-        public ICollection<EventEquipment>? EventEquipment { get; set; }
+        public ICollection<EventEquipment> EventEquipments { get; set; } = new List<EventEquipment>();
     }
 }
