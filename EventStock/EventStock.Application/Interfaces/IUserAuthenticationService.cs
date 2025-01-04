3c69bec6d4adb89d91ad5d232e56a08db53ffa74
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-04 23:29:05 +0100

    Change name of the IAuthenticationService.cs file

---
+++ EventStock/EventStock.Application/Interfaces/IUserAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using EventStock.Application.Dto.User;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventStock.Application.Interfaces
+{
+    public interface IUserAuthenticationService
+    {
+        Task Login(LoginUserDto user);
+        Task<SignInResult> AuthenticateUser(LoginUserDto user);
+        string GenerateJWT(LoginUserDto user);
+    }
+}
