2b6691b285e850e998f63ec2fe61f8c8154e8085
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-07 13:38:17 +0100

    Update IUserService

--- EventStock/EventStock.Application/Interfaces/IUserService.cs
+++ EventStock/EventStock.Application/Interfaces/IUserService.cs
@@ -1,13 +1,14 @@
 ﻿using EventStock.Application.Dto.Stock;
 using EventStock.Application.Dto.User;
+using Microsoft.AspNetCore.Identity;
 
 
 namespace EventStock.Application.Interfaces
 {
     public interface IUserService
     {
-        Task CreateUserAsync(CreateUserDto userDto);
-        Task<UserDto> GetUserAsync(int id);
+        Task<IdentityResult> CreateUserAsync(CreateUserDto userDto);
+        Task<UserDto> GetUserAsync(string id);
         Task<UserDto> UpdateUserAsync(UserDto user);
         Task DeleteUserAsync(int id);
         Task<List<StockDto>> ListUsersStocksAsync(int id);
