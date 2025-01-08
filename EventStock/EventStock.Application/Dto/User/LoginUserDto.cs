0407092378bc5204acd409ab9301c6a2ad949f20
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-08 14:48:55 +0100

    Add [EmailAddress] DataAnnotation

--- EventStock/EventStock.Application/Dto/User/LoginUserDto.cs
+++ EventStock/EventStock.Application/Dto/User/LoginUserDto.cs
@@ -5,6 +5,7 @@ namespace EventStock.Application.Dto.User
     public class LoginUserDto
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
