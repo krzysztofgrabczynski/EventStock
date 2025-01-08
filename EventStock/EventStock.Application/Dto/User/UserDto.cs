fc869272da5af89cd2a590847fbceaa034661707
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-08 14:48:25 +0100

    Add all fields as optional

--- EventStock/EventStock.Application/Dto/User/UserDto.cs
+++ EventStock/EventStock.Application/Dto/User/UserDto.cs
@@ -5,9 +5,9 @@ namespace EventStock.Application.Dto.User
 {
     public class UserDto : IMapFrom<EventStock.Domain.Models.User>
     {
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
 
         public void Mapping(Profile profile)
         {
