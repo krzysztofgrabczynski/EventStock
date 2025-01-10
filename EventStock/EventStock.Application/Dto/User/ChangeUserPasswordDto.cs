76e04df192b7f6b4a5b241be7dc229b27aad1e6e
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-10 22:09:56 +0100

    Create DTO class for changing password by user

---
+++ EventStock/EventStock.Application/Dto/User/ChangeUserPasswordDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventStock.Application.Dto.User
+{
+    public class ChangeUserPasswordDto
+    {
+        [Required]
+        public string OldPassword { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords must be the same!")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
