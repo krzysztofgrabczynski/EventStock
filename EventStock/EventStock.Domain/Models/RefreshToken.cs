73e30fae0644c8c7b189b7379a24782ee52e1ea7
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-10 22:02:59 +0100

    Craete new model - TokenRefresh

---
+++ EventStock/EventStock.Domain/Models/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace EventStock.Domain.Models
+{
+    public class RefreshToken
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string UserId { get; set; }
+        public required string Token { get; set; }
+        public DateTime CreationAt { get; set; } = DateTime.UtcNow;
+        public required DateTime Expiration { get; set; }
+    }
+}
