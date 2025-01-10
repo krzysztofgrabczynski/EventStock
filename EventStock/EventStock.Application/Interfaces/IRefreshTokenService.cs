8ddcf250c7f4813a49bc7c9466f3969b6eed5629
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-10 22:08:23 +0100

    Add services for refresh token

--- EventStock/EventStock.Application/Interfaces/IRefreshTokenService.cs
+++ EventStock/EventStock.Application/Interfaces/IRefreshTokenService.cs
@@ -1,7 +1,12 @@
-﻿namespace EventStock.Application.Interfaces
+﻿using EventStock.Domain.Models;
+
+namespace EventStock.Application.Interfaces
 {
     public interface IRefreshTokenService
     {
         Task<string> GenerateRefreshTokenAsync(string userId);
+        Task<RefreshToken> GetRefreshTokenAsync(string userId);
+        bool CheckRefreshToken(RefreshToken tokneFromDB, string tokenFromRequest);
+        Task DeleteRefreshTokenAsync(string userId);
     }
 }
--- EventStock/EventStock.Application/Services/RefreshTokenService.cs
+++ EventStock/EventStock.Application/Services/RefreshTokenService.cs
@@ -1,5 +1,8 @@
 ﻿using EventStock.Application.Interfaces;
 using EventStock.Domain.Interfaces;
+using EventStock.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -7,9 +10,11 @@ namespace EventStock.Application.Services
 {
     public class RefreshTokenService : IRefreshTokenService
     {
+        private readonly IConfiguration _configuration;
         private readonly IRefreshTokenRepository _refreshTokenRepository;
-        public RefreshTokenService(IRefreshTokenRepository refreshTokenRepository)
+        public RefreshTokenService(IConfiguration configuration, IRefreshTokenRepository refreshTokenRepository)
         {
+            _configuration = configuration;
             _refreshTokenRepository = refreshTokenRepository;
         }
 
@@ -18,7 +23,14 @@ namespace EventStock.Application.Services
             var refreshToken = Guid.NewGuid().ToString();
 
             var hashedRefreshToken = HashToken(refreshToken);   
-            await _refreshTokenRepository.SaveRefreshTokenAsync(userId, refreshToken);
+            var expirationTime = DateTime.UtcNow.AddDays(int.Parse(_configuration["RefreshToken:ExpirationTimeInDays"]));
+            var refreshTokenToSave = new RefreshToken()
+            {
+                UserId = userId,
+                Token = hashedRefreshToken,
+                Expiration = expirationTime,
+            };
+            await _refreshTokenRepository.SaveRefreshTokenAsync(refreshTokenToSave);
             
             return refreshToken;
         }
@@ -28,6 +40,34 @@ namespace EventStock.Application.Services
             using var sha256 = SHA256.Create();
             var bytes = Encoding.UTF8.GetBytes(token);
             return Convert.ToBase64String(sha256.ComputeHash(bytes));
+        }
+
+        public async Task<RefreshToken> GetRefreshTokenAsync(string userId)
+        {
+            var token = await _refreshTokenRepository.GetRefreshTokenAsync(userId);
+            if (token == null)
+            {
+                throw new SecurityTokenValidationException("Invalid token");
+            }
+            return token;
+        }
+
+        public async Task DeleteRefreshTokenAsync(string userId)
+        {
+            await _refreshTokenRepository.DeleteRefreshTokenAsync(userId);
+        }
+
+        public bool CheckRefreshToken(RefreshToken tokenFromDB, string tokenFromRequest)
+        {
+            var hashedTokenFromRequest = HashToken(tokenFromRequest);
+            var hashedTokenFromDB = tokenFromDB.Token;
+
+            if (hashedTokenFromDB != hashedTokenFromRequest || tokenFromDB.Expiration < DateTime.UtcNow)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
