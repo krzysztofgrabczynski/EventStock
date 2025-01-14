768cf1d3b735ffc72ec96605bea218d37c93bedb
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-15 00:11:20 +0100

    Add errors to handle failure result pattern situations

---
+++ EventStock/EventStock.Application/ResultPattern/Errors/BaseResultError.cs
@@ -0,0 +1,7 @@
+﻿namespace EventStock.Application.ResultPattern.Errors
+{
+    public record ResultError(string Code, string Description)
+    {
+        public static readonly ResultError None = new(string.Empty, string.Empty);
+    }
+}
---
+++ EventStock/EventStock.Application/ResultPattern/Errors/UserResultErrors.cs
@@ -0,0 +1,7 @@
+﻿namespace EventStock.Application.ResultPattern.Errors
+{
+    public record UserNotFoundResultError : ResultError
+    {
+        public UserNotFoundResultError() : base("UserNotFound", "User with provided ID not found") { }
+    }
+}
