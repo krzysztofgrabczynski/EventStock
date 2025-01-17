606b47949e69b05858f3f044ee2c326825d75499
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-17 22:43:12 +0100

    Update token management tests

--- EventStock/EventStock.Tests/TokenManagementTest.cs
+++ EventStock/EventStock.Tests/TokenManagementTest.cs
@@ -1,59 +1,121 @@
 ﻿using EventStock.Application.Services;
-using Microsoft.AspNetCore.Http;
+using EventStock.Domain.Interfaces;
+using EventStock.Domain.Models;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using System.IdentityModel.Tokens.Jwt;
+
 
 namespace EventStock.Tests
 {
     public class TokenManagementTest
     {
         private readonly Mock<IConfiguration> _configurationMock;
-        //private readonly JwtTokenService _jwtTokenService;
+        private readonly TokenManagementService _tokenManagementService;
+        private readonly Mock<IRefreshTokenRepository> _refreshTokenRepositoryMock;
 
         public TokenManagementTest()
         {
             _configurationMock = new Mock<IConfiguration>();
-            //_jwtTokenService = new JwtTokenService(_configurationMock.Object);
+            _refreshTokenRepositoryMock = new Mock<IRefreshTokenRepository>();
+            _tokenManagementService = new TokenManagementService(_configurationMock.Object, _refreshTokenRepositoryMock.Object);
         }
 
-        //[Fact]
-        //public void GenerateJwtTest()
-        //{
-        //    // Arrange
-        //    string id = Guid.NewGuid().ToString();
-        //    _configurationMock.Setup(c => c["Jwt:Key"]).Returns("supersecretkeyforsigningsupersecretkeyforsigning");
-        //    _configurationMock.Setup(c => c["Jwt:Issuer"]).Returns("TestIssuer");
-        //    _configurationMock.Setup(c => c["Jwt:Audience"]).Returns("TestAudience");
-        //    _configurationMock.Setup(c => c["Jwt:ExpirationTime"]).Returns("60");
+        [Fact]
+        public async Task GenerateTokensAsyncTest()
+        {
+            // Arrange
+            string id = Guid.NewGuid().ToString();
+            _configurationMock.Setup(c => c["Jwt:Key"]).Returns("supersecretkeyforsigningsupersecretkeyforsigning");
+            _configurationMock.Setup(c => c["Jwt:Issuer"]).Returns("TestIssuer");
+            _configurationMock.Setup(c => c["Jwt:Audience"]).Returns("TestAudience");
+            _configurationMock.Setup(c => c["Jwt:ExpirationTime"]).Returns("60");
 
-        //    // Act
-        //    var token = _jwtTokenService.GenerateJWT(id);
-        //    var tokenHandler = new JwtSecurityTokenHandler();
-        //    var jwtToken = tokenHandler.ReadJwtToken(token);
+            var refreshTokenDto = new RefreshToken()
+            {
+                UserId = Guid.NewGuid().ToString(),
+                Token = "hashed-refresh-token",
+                Expiration = DateTime.UtcNow.AddDays(7)
+            };
+            _configurationMock.Setup(c => c["RefreshToken:ExpirationTimeInDays"]).Returns("7");
 
-        //    // Assert
-        //    Assert.NotNull(jwtToken);
-        //    Assert.Equal(id, jwtToken.Subject);
-        //    Assert.Equal("TestIssuer", jwtToken.Issuer);
-        //    Assert.Contains("TestAudience", jwtToken.Audiences);
-        //}
+            // Act
+            var tokens = await _tokenManagementService.GenerateTokensAsync(id);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtToken = tokenHandler.ReadJwtToken(tokens.Value.AccessToken);
+            var refreshToken = tokens.Value.RefreshToken;
 
-        //[Fact]
-        //public void GetIdFromJwtTokenTest()
-        //{
-        //    // Arrange
-        //    var jwtToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwYmM5YjBkMC1lMmFhLTQ4ZjYtOWFkYy04NTQwZTY4NDEzOGMiLCJzdWIiOiJ1c2VyLWlkIiwibmJmIjoxNzM2NzcxOTMwLCJleHAiOjE3MzY3NzU1MzAsImlhdCI6MTczNjc3MTkzMCwiaXNzIjoidGVzdC1pc3N1ZXIiLCJhdWQiOiJ0ZXN0LWF1ZGllbmNlIn0.CcsE5L31ukNgvp7pxlxaLG0UwQGrFyW1Y-Xh2Jn3qEk";
-        //    var sub = "user-id";
-        //    var mock = new Mock<IHeaderDictionary>();
-        //    mock.Setup(m => m["Authorization"]).Returns(jwtToken);
+            // Assert
+            Assert.NotNull(jwtToken);
+            Assert.Equal(id, jwtToken.Subject);
+            Assert.Equal("TestIssuer", jwtToken.Issuer);
+            Assert.Contains("TestAudience", jwtToken.Audiences);
 
-        //    // Act
-        //    var idFromToken = _jwtTokenService.GetIdFromJwtToken(mock.Object);
+            Assert.NotNull(refreshToken);
+            Assert.NotEqual(refreshTokenDto.Token, refreshToken);
+        }
 
-        //    // Assert
-        //    Assert.NotNull(idFromToken);
-        //    Assert.Equal(idFromToken, sub);
-        //}     
+        [Fact]
+        public async Task GetRefreshTokenAsyncTest()
+        {
+            // Arrange
+            var refreshToken = "test-refresh-token";
+            var returned = new RefreshToken()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserId = "test-user-id",
+                Token = refreshToken,
+                CreationAt = DateTime.UtcNow,
+                Expiration = DateTime.UtcNow.AddDays(7)
+            };
+            _refreshTokenRepositoryMock.Setup(m => m.GetRefreshTokenAsync(refreshToken)).ReturnsAsync(returned);
+
+            // Act
+            var result = await _tokenManagementService.GetRefreshTokenAsync(refreshToken, needHash: false);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result.Value.Token, refreshToken);
+        }
+
+        [Fact]
+        public void CheckIfRefreshTokenExpiredPositiveTest()
+        {
+            // Arrange
+            var refreshToken = new RefreshToken()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserId = "test-user-id",
+                Token = "refresh-token-test",
+                CreationAt = DateTime.UtcNow,
+                Expiration = DateTime.UtcNow.AddDays(1)
+            };
+
+            // Act
+            var result = _tokenManagementService.CheckRefreshTokenExpiration(refreshToken);
+
+            // Assert
+            Assert.True(result.Succeeded);
+        }
+
+        [Fact]
+        public void CheckIfRefreshTokenExpiredNegativeTest()
+        {
+            // Arrange
+            var refreshToken = new RefreshToken()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserId = "test-user-id",
+                Token = "refresh-token-test",
+                CreationAt = DateTime.UtcNow,
+                Expiration = DateTime.UtcNow.AddDays(-1)
+            };
+
+            // Act
+            var result = _tokenManagementService.CheckRefreshTokenExpiration(refreshToken);
+
+            // Assert
+            Assert.False(result.Succeeded);
+        }
     }
 }
