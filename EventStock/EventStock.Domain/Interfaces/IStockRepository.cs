fcec403b0bfd3098a4b64559a764cc5eb4f9acb1
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-20 10:31:41 +0100

    Create interface for stock repository - operation on stock in db

---
+++ EventStock/EventStock.Domain/Interfaces/IStockRepository.cs
@@ -0,0 +1,12 @@
+﻿using EventStock.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventStock.Infrastructure.Repositories
+{
+    public interface IStockRepository
+    {
+        Task AddUserAsync(Stock stock, User user, IdentityRole role);
+        Task<int?> CreateStockAsync(Stock stock);
+        Task<Stock?> GetStockAsync(int id);
+    }
+}
