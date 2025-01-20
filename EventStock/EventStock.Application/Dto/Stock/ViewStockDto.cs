82884211155950cc7eebf84fb41f02524af96e9f
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-20 10:44:03 +0100

    Update ViewStockDto

--- EventStock/EventStock.Application/Dto/Stock/ViewStockDto.cs
+++ EventStock/EventStock.Application/Dto/Stock/ViewStockDto.cs
@@ -1,6 +1,18 @@
-﻿namespace EventStock.Application.Dto.Stock
+﻿using AutoMapper;
+using EventStock.Application.Mapping;
+using EventStock.Domain.Models;
+
+namespace EventStock.Application.Dto.Stock
 {
-    public class ViewStockDto
+    public class ViewStockDto : IMapFrom<EventStock.Domain.Models.Stock>
     {
+        public required string Name { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<UserWithRoleDto> Users { get; set; } = new List<UserWithRoleDto>();
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<EventStock.Domain.Models.Stock, ViewStockDto>();
+        }
     }
 }
