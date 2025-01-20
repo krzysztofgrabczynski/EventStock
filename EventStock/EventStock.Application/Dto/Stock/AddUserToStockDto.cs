363189f1685cacc71294807351cc32154958e384
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-20 10:42:53 +0100

    Create new dtos for stock

---
+++ EventStock/EventStock.Application/Dto/Stock/AddUserToStockDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventStock.Application.Dto.Stock
+{
+    public class AddUserToStockDto
+    {
+        [Required]
+        public int StockId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        
+        public string? Role { get; set; }
+    }
+}
---
+++ EventStock/EventStock.Application/Dto/Stock/CreateStockDto.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EventStock.Application.Mapping;
+using EventStock.Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventStock.Application.Dto.Stock
+{
+    public class CreateStockDto : IMapFrom<Domain.Models.Stock>
+    {
+        [Required]
+        public string Name { get; set; }
+        public Address? Address{ get; set; }
+        public ICollection<Domain.Models.User> Users { get; set; } = new List<Domain.Models.User>();
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateStockDto, Domain.Models.Stock>();
+        }
+    }
+}
