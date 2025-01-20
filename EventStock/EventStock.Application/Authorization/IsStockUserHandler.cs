56c378197c45cfbea57296a83f80668af3a42950
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-20 10:34:21 +0100

    Create handler for IsStockUserRequirement that check if the user can access to specific resource - if is a stock member

---
+++ EventStock/EventStock.Application/Authorization/IsStockUserHandler.cs
@@ -0,0 +1,25 @@
+﻿using EventStock.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace EventStock.Application.Authorization
+{
+    public class IsStockUserHandler : AuthorizationHandler<IsStockUserRequirement, Stock>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsStockUserRequirement requirement, Stock resource)
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (!resource.UserStockRoles.Any(usr => usr.User.Id == userId && usr.Role.Name == "StockAdmin"))
+            {
+                context.Fail();
+            }
+            else
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
