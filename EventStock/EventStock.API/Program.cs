41d2834b1420a3454f330479468f7a9b9d4e3950
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-29 23:23:59 +0100

    CorsPolicy refactor

--- EventStock/EventStock.API/Program.cs
+++ EventStock/EventStock.API/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policyBuilder =>
     {
-    policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5174");
+    policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173");
     });
 });
 
