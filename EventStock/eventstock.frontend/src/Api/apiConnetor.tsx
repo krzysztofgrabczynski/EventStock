4eca4a892e94b93ebd5d5ea8050dfee20c8e6f44
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-29 23:32:04 +0100

    Create api connector for auth endpoints

---
+++ EventStock/eventstock.frontend/src/Api/apiConnetor.tsx
@@ -0,0 +1,44 @@
+import axios from "axios"
+import { API_BASE_URL } from "./config"
+import { LoginRequest } from "../Models/Auth/LoginRequest";
+import { LoginResponse } from "../Models/Auth/LoginResponse";
+import { RegisterRequest } from "../Models/Auth/RegisterRequest";
+import { RegisterResponse } from "../Models/Auth/RegisterResponse";
+
+export const loginAPI = async (request: LoginRequest) => {
+    try {
+        const data = await axios.post<LoginResponse>(API_BASE_URL + "Authenticate/Login", {
+            email: request.email,
+            password: request.password
+        });
+        return data;
+    } catch (error) {
+        console.log("error message: ", error);
+        return error;
+    }
+}
+
+export const registerAPI = async (request: RegisterRequest) => {
+    try {
+        const data = await axios.post<RegisterResponse>(API_BASE_URL + "Authenticate/Register", {
+            email: request.email,
+            password: request.password,
+            confirmPassword: request.confirmPassword,
+        });
+        return data;
+    } catch (error) {
+        console.log("error message: ", error);
+        return error
+    }
+}
+
+export const logoutAPI = async (refreshToken: string) => {
+    try {
+        await axios.post<void>(API_BASE_URL + "Authenticate/Logout", {
+            refreshToken: refreshToken,
+        });
+    } catch (error) {
+        console.log("error message: ", error);
+        return error;
+    }
+}
