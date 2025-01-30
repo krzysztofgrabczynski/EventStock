afdefcb8e97c21b4dc271346bec4069623ea73f7
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-01-30 12:12:36 +0100

    Add navbar to the App.tsx

--- EventStock/eventstock.frontend/src/App.tsx
+++ EventStock/eventstock.frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Outlet } from 'react-router';
 import { AuthProvider } from './Context/useAuth';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
     return (
         <>
             <AuthProvider>
+                <Navbar />
                 <Outlet />
             </AuthProvider>
         </>
