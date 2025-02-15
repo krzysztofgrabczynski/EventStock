a06a15ff3fcf6a55fcb5dd89749186f1f2607835
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2025-02-16 00:42:23 +0100

    Add Roles prop to the UserDto

--- EventStock/EventStock.Application/Dto/User/UserDto.cs
+++ EventStock/EventStock.Application/Dto/User/UserDto.cs
@@ -8,10 +8,12 @@ namespace EventStock.Application.Dto.User
         public string? Email { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<EventStock.Domain.Models.User, UserDto>();
+            profile.CreateMap<EventStock.Domain.Models.User, UserDto>()
+                .ForMember(dest => dest.Roles, opt => opt.Ignore());
         }
     }
 }
